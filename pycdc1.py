be48a5224a094f9c8702a978eae5a94e7a69d3b1
cloudaice <cloudaice@163.com>
2012-01-16 12:53:32 +0800

    修改了一些内部提示文字

--- pycdc1.py
+++ pycdc1.py
@@ -9,16 +9,16 @@ class PYCDC(cmd.Cmd):
         self.CDDIR='cdc/'
         self.prompt='(pycdc)>'
         self.intro='''PYCDC0.5使用说明：
-        dir 目录名 #指定保存目录和搜索目录名 默认是'cdc'
-        walk 文件名 使用"*.cdc"
-        find 关键字  搜索遍历目录中左右.cdc文件输出含有关键字的行
-        ? 查询
-        exit 推出系统
+        dir    目录名     指定保存目录和保存的文件名 默认是'cdc'
+        walk   文件名     使用"*.cdc"
+        find   关键字     搜索遍历目录中的.cdc文件输出含有关键字的行
+        ?      查询帮助
+        exit   退出系统
         '''
 
         
     def help_exit(self):
-        print '推出程序 quits the program'
+        print '退出程序 quits the program'
     def do_exit(self,line):
         sys.exit()
     
@@ -33,8 +33,8 @@ class PYCDC(cmd.Cmd):
         print "指定保存/搜索目录"
     def do_dir(self,pathname):
         if pathname=="":pathname=raw_input("输入指定保存/搜索目录:")
-        self.CDDIR=pathname
         print "指定保存搜索目录是 ‘%s';默认是：'%s'" % (pathname,self.CDDIR)
+        self.CDDIR=pathname
 
     def help_find(self):
         print "搜索关键字"
