27664994eb4f0c2a7cf6abdbafd44262d191d15b
cloudaice <cloudaice@163.com>
2012-01-18 21:41:52 +0800

    终于可以基本完整使用了，检索出来的目录打印在文件里面，显得更加整洁格式化。容易看清。另外正对的中文编码问题也很好的解决了

--- cdctools.py
+++ cdctools.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 import os
 import sys
+import chardet
 def cdwalker(cdrom,cdfile):
     export=""
     for root,dirs,files in os.walk(cdrom):
-        export+="\n %s;%s;%s" %(root,dirs,files)
+        export+=formatinfo(root,dirs,files)
     open(cdfile,"w").write(export)
 
 def cdcgrep(filepath,keyword):
@@ -15,6 +16,21 @@ def cdcgrep(filepath,keyword):
             for line in cdcfile.readlines():
                 if keyword in line:
                     print line
-if __name__ == '__main__':
-    CDROM="/media/cdrom0"
+
+def _smartcode(stream):
+    """smart stream into utf-8"""
+    codedetect = chardet.detect(stream)["encoding"]
+    ustring = unicode(stream,codedetect)
+    return ustring.encode('utf-8')
     
+def formatinfo(root,dirs,files):
+    export = "\n"+root+"\n"
+    for d in dirs:
+        export+="-d %s %s \n" % (root,_smartcode(d))
+    for f in files:
+        export+="-f %s %s \n" % (root,_smartcode(f))
+    export+= "="*100
+    return export
+
+if __name__ == '__main__':
+    cdwalker("/media/cdrom0",'s.cdc')
--- pycdc1.py
+++ pycdc1.py
@@ -9,7 +9,7 @@ class PYCDC(cmd.Cmd):
         self.CDDIR='cdc/'
         self.prompt='(pycdc)>'
         self.intro='''PYCDC0.5使用说明：
-        dir       目录名     指定保存目录和保存的文件名 默认是'cdc/'
+        dir       目录名     指定保存目录或者搜索目录 默认是'cdc/'
         walk      文件名     使用"*.cdc" 将扫描得到的目录放到该文件夹中
         find      关键字     搜索遍历目录中的.cdc文件输出含有关键字的行
         diskdir   目录名     指定要扫描的路径名和光盘名 默认是"/media/cdrom0"
@@ -20,36 +20,53 @@ class PYCDC(cmd.Cmd):
         
     def help_exit(self):
         print '退出程序 quits the program'
+
     def do_exit(self,line):
         sys.exit()
     
+
     def help_diskdir(self):
         print "指定要扫描的光盘路径名以及光盘名"
+
     def do_diskdir(self,diskdir):
-        if diskdir=='': diskdir=raw_input("输入路径名及光盘名")
+        if diskdir=='': 
+            diskdir=raw_input("输入路径名及光盘名\n")
         print "您输入的光盘名及路径名是 %s" % diskdir
         self.CDROM=diskdir
 
+
     def help_walk(self):
         print "扫描光盘内容 walk cd and export into *.cdc"
+
     def do_walk(self,filename):
-        if filename=="": filename= raw_input("输入文件名:cdc::")
+        if filename=="":
+            filename= raw_input("输入文件名:cdc:: ")
         print "扫描光盘内容保存到:'%s'" % filename
         cdwalker(self.CDROM,self.CDDIR+filename)
 
+
     def help_dir(self):
         print "指定保存/搜索目录"
+
     def do_dir(self,pathname):
-        if pathname=="":pathname=raw_input("输入指定保存/搜索目录:")
+        if pathname=="":
+            pathname=raw_input("输入指定保存/搜索目录:")
         print "指定保存搜索目录是 ‘%s';默认是：'%s'" % (pathname,self.CDDIR)
         self.CDDIR=pathname
 
+
     def help_find(self):
         print "搜索关键字"
+
     def do_find(self,keyword):
-        if keyword=="":keyword=raw_input("输入搜索关键字:")
+        if keyword=="":
+            keyword=raw_input("输入搜索关键字:")
         print "搜索关键词: '%s'" % keyword
         cdcgrep(self.CDDIR,keyword)
+
+    do_q=do_exit
+
+
 if __name__== '__main__':
     cdc=PYCDC()
     cdc.cmdloop()
